38eec3aae22faf8f0ec3dc77b79fbaf0e6b266ff
YuikoIto <toffywing@gmail.com>
2021-05-30 06:00:26 +0900

    feat::tada: responsive

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,6 +2,7 @@ import useToggle from "hooks/useToggle";
 import { useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import "styles/components/common/header.scss";
+import clsx from "clsx";
 
 const Header = () => {
   const pages = ["Loader", "Button"];
@@ -19,6 +20,14 @@ const Header = () => {
   return (
     <header className="header">
       <h1 className="title">UI Components</h1>
+      <div
+        className={clsx("sp-button", isOpen && "-active")}
+        onClick={clickHandler}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
       <div className="button" onClick={clickHandler}>
         {page}
       </div>
