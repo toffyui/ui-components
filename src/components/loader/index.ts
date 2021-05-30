b45014b6614776c77d14be41c07a53214966d73c
YuikoIto <toffywing@gmail.com>
2021-05-30 16:10:19 +0900

    feat::tada: add loader5

--- src/components/loader/index.ts
+++ src/components/loader/index.ts
@@ -2,6 +2,13 @@ import { loader1 } from "components/loader/loader1";
 import { loader2 } from "components/loader/loader2";
 import { loader3 } from "components/loader/loader3";
 import { loader4 } from "components/loader/loader4";
+import { loader5 } from "components/loader/loader5";
 import { styleModel } from "models/styleModel";
 
-export const loaders: styleModel[] = [loader1, loader2, loader3, loader4];
+export const loaders: styleModel[] = [
+  loader1,
+  loader2,
+  loader3,
+  loader4,
+  loader5,
+];
