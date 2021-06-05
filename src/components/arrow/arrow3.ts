ef6d09233fb85152cfafdf13873d653669dd58b3
YuikoIto <toffywing@gmail.com>
2021-06-06 01:39:48 +0900

    chore::+1: fix style for arrow3

--- src/components/arrow/arrow3.ts
+++ src/components/arrow/arrow3.ts
@@ -3,31 +3,28 @@ import { styleModel } from "models/styleModel";
 export const arrow3: styleModel = {
   title: "arrow3",
   scss: `.arrow3 {
-  margin: 100px auto;
+  margin: 95px auto;
   position: relative;
+  width: 70px;
+  height: 10px;
+  background: #333c5f;
+  border-radius: 10px;
   &::before,
   &::after {
     position: absolute;
     content: "";
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    margin: auto;
-  }
-  &::before {
-    left: -20px;
     width: 50px;
     height: 10px;
+    border-radius: 10px;
     background: #333c5f;
   }
-  &::after {
-    width: 30px;
-    height: 30px;
-    border-top: 10px solid #333c5f;
-    border-right: 10px solid #333c5f;
-    -webkit-transform: rotate(45deg);
+  &::before {
+    top: calc(50% - 20px);
     transform: rotate(45deg);
+  }
+  &::after {
+    bottom: calc(50% - 20px);
+    transform: rotate(-45deg);
   }
 }
 `,
